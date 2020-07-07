1ea6ebcf30786b19a8c14e0a835809e80ec9856e
remoncr7 <remoncr77@gmail.com>
2020-07-07 23:40:43 +0900

    リファクタリング

--- app/uploaders/img_uploader.rb
+++ app/uploaders/img_uploader.rb
@@ -31,7 +31,7 @@ class ImgUploader < CarrierWave::Uploader::Base
   # end
   # Create different versions of your uploaded files:
   version :thumb do
-    process resize_to_fit: [350, 350]
+    process resize_to_fit: [500, 500]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
