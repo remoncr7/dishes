a8c84f76edda9b81ef346d74eefdd8bd0e9e090b
remoncr7 <remoncr77@gmail.com>
2020-07-09 11:29:17 +0900

    showアクションと参考URLのstyle適用

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
   validates :title, presence: true, length: { maximum: 15, too_long: "は%{count}文字以内で設定してください "}
   validates :img, presence: true
+  validates :url, {:allow_blank => true, format: /\A#{URI::regexp(%w(http https))}\z/}
   belongs_to :user
   mount_uploader :img, ImgUploader
 end
