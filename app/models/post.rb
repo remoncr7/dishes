cc69d9281e69754aeb619b176772b0e0315c7474
remoncr7 <remoncr77@gmail.com>
2020-07-12 15:03:03 +0900

    キャプションにバリデーション追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
   validates :title, presence: true, length: { maximum: 15, too_long: "は%{count}文字以内で設定してください "}
   validates :img, presence: true
+  validates :text, length: { maximum: 400, too_long: "は%{count}文字以内で設定してください "}
   validates :url, {:allow_blank => true, format: /\A#{URI::regexp(%w(http https))}\z/}
   belongs_to :user
   mount_uploader :img, ImgUploader
