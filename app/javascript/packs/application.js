d1bb4d50c69e54f9f39d859528ed66592d2c46c5
remoncr7 <remoncr77@gmail.com>
2020-07-15 16:39:04 +0900

    スタイルのリファクタリング

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,10 +7,8 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-// require("jquery")
-// require("bootstrap")
+
 import 'bootstrap';
-// import '../src/application.scss'
 import "src/js/application.js";
 import "src/css/application.scss";
 // Uncomment to copy all static images under ../images to the output folder and reference
